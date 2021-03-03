52640cf9412c2a7762cd77d791e59e2b0954c3d4
q40603 <q40603@gmail.com>
2021-03-03 13:02:56 +0800

    hw1/q1 : check file not found

--- hw1/q40603/countline.sh
+++ hw1/q40603/countline.sh
@@ -1,6 +1,10 @@
 if [ $# -eq 1  ]; then
-    lines=$(awk 'END{print NR}' "$1")
-    echo "$lines lines in $1";
+    if [ -e $1 ]; then
+        lines=$(awk 'END{print NR}' "$1")
+        echo "$lines lines in $1";
+    else
+        echo "$1 not found"
+    fi
 elif [ $# -eq 0 ]; then
     echo "missing file name";
 else
