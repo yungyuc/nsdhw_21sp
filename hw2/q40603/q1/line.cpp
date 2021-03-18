138f0d4e06a268c0c6f648283e588d01f0811980
q40603 <q40603@gmail.com>
2021-03-18 04:25:08 +0000

    finish line class

--- hw2/q40603/q1/line.cpp
+++ hw2/q40603/q1/line.cpp
@@ -6,19 +6,21 @@ class Line
 {
 public:
     Line() {};
-    Line(Line const & l1){
-        m_size = l1.m_size;
-        m_x = l1.m_x;
-        m_y = l1.m_y;        
-    };
-    Line(Line       &&);
+    Line(Line const & l1): m_size(l1.m_size), m_x(l1.m_x), m_y(l1.m_y){};
+    Line(Line       && l1): m_size(l1.m_size), m_x(std::move(l1.m_x)), m_y(std::move(l1.m_y)){};
     Line & operator=(Line const & l1){
         m_size = l1.m_size;
-        m_x = l1.m_x;
-        m_y = l1.m_y;
+        m_x.assign(l1.m_x.begin(), l1.m_x.end());
+        m_y.assign(l1.m_y.begin(), l1.m_y.end());
     };
-    Line & operator=(Line       &&);
-    Line(size_t size) {m_size=size; m_x.reserve(m_size); m_y.reserve(m_size);};
+    Line & operator=(Line && l1) {
+        if (this == &l1) { return *this; }
+        m_size = l1.m_size;
+        m_x = std::move(l1.m_x);
+        m_y = std::move(l1.m_y);
+        return *this;
+    };
+    Line(size_t size): m_size(size),m_x(m_size,0),m_y(m_size,0){};
     ~Line() {};
     size_t size() const {return m_size;};
     float const & x(size_t it) const{return m_x[it];};
@@ -40,7 +42,7 @@ int main(int, char **)
     line.x(2) = 2; line.y(2) = 5;
 
     Line line2(line);
-    // line2.x(0) = 9;
+    line2.x(0) = 9;
 
     std::cout << "line: number of points = " << line.size() << std::endl;
     for (size_t it=0; it<line.size(); ++it)
