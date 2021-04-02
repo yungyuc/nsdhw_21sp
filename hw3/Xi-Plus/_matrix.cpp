2eb36aced0903f76c10ab835fd698a4cd8355231
xiplus <huangxuanyuxiplus@gmail.com>
2021-04-02 17:02:36 +0800

    Meet vaildate.py requirements

--- hw3/Xi-Plus/_matrix.cpp
+++ hw3/Xi-Plus/_matrix.cpp
@@ -1,6 +1,7 @@
 // Ref: https://pybind11.readthedocs.io/en/stable/basics.html#creating-bindings-for-a-simple-function
 #include <mkl.h>
 #include <pybind11/numpy.h>
+#include <pybind11/operators.h>
 #include <pybind11/pybind11.h>
 
 #include <iostream>
@@ -9,8 +10,11 @@
 // Ref: https://github.com/yungyuc/nsd/blob/master/notebook/current/05_matrix/matrix.ipynb
 class Matrix {
    public:
-	Matrix(size_t nrow, size_t ncol)
-		: m_nrow(nrow), m_ncol(ncol) {
+	size_t nrow;
+	size_t ncol;
+
+	Matrix(size_t _nrow, size_t _ncol)
+		: nrow(_nrow), ncol(_ncol) {
 		size_t nelement = nrow * ncol;
 		m_buffer = new double[nelement];
 		memset(m_buffer, 0, nrow * ncol * sizeof(double));
@@ -22,37 +26,37 @@ class Matrix {
 
 	// No bound check.
 	double* data() const { return m_buffer; }
-	double operator()(size_t row, size_t col) const { return m_buffer[row * m_ncol + col]; }
-	double& operator()(size_t row, size_t col) { return m_buffer[row * m_ncol + col]; }
+	double operator()(size_t row, size_t col) const { return m_buffer[row * ncol + col]; }
+	double& operator()(size_t row, size_t col) { return m_buffer[row * ncol + col]; }
 
 	// Ref: https://pybind11.readthedocs.io/en/stable/advanced/pycpp/numpy.html
 	void load(pybind11::array_t<double> input) {
 		pybind11::buffer_info buf = input.request();
 
-		memcpy(m_buffer, buf.ptr, m_nrow * m_ncol * sizeof(double));
+		memcpy(m_buffer, buf.ptr, nrow * ncol * sizeof(double));
 		// for (size_t i = 0; i < size; i++) {
 		// 	m_buffer[i] = buf[i];
 		// }
 	}
 
 	void load2(double* input) {
-		memcpy(m_buffer, input, m_nrow * m_ncol * sizeof(double));
+		memcpy(m_buffer, input, nrow * ncol * sizeof(double));
 	}
 
 	std::string asstring() const {
 		std::stringstream ss;
 		ss << "[";
-		for (size_t row = 0; row < m_nrow; row++) {
+		for (size_t row = 0; row < nrow; row++) {
 			if (row > 0) {
 				ss << " ";
 			}
 
 			ss << "[";
-			for (size_t col = 0; col < m_ncol; col++) {
+			for (size_t col = 0; col < ncol; col++) {
 				ss << (*this)(row, col) << " ";
 			}
 			ss << "]";
-			if (row < m_nrow - 1) {
+			if (row < nrow - 1) {
 				ss << std::endl;
 			}
 		}
@@ -61,24 +65,30 @@ class Matrix {
 		return ss.str();
 	}
 
-	size_t nrow() const { return m_nrow; }
-	size_t ncol() const { return m_ncol; }
+	bool operator==(const Matrix& rhs) const {
+		for (size_t row = 0; row < nrow; row++) {
+			for (size_t col = 0; col < ncol; col++) {
+				if ((*this)(row, col) != rhs(row, col)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 
    private:
-	size_t m_nrow;
-	size_t m_ncol;
 	double* m_buffer;
 };
 
 Matrix multiply_naive(Matrix& ma, Matrix& mb) {
-	if (ma.ncol() != mb.nrow()) {
+	if (ma.ncol != mb.nrow) {
 		throw pybind11::value_error("Given matrices cannot be multiplied");
 	}
 
-	Matrix res(ma.nrow(), mb.ncol());
-	for (size_t row = 0; row < ma.nrow(); row++) {
-		for (size_t col = 0; col < mb.ncol(); col++) {
-			for (size_t i = 0; i < ma.ncol(); i++) {
+	Matrix res(ma.nrow, mb.ncol);
+	for (size_t row = 0; row < ma.nrow; row++) {
+		for (size_t col = 0; col < mb.ncol; col++) {
+			for (size_t i = 0; i < ma.ncol; i++) {
 				res(row, col) += ma(row, i) * mb(i, col);
 			}
 		}
@@ -87,27 +97,27 @@ Matrix multiply_naive(Matrix& ma, Matrix& mb) {
 	return res;
 }
 
-Matrix multiple_tile(Matrix& ma, Matrix& mb, size_t tsize) {
-	if (ma.ncol() != mb.nrow()) {
+Matrix multiply_tile(Matrix& ma, Matrix& mb, size_t tsize) {
+	if (ma.ncol != mb.nrow) {
 		throw pybind11::value_error("Given matrices cannot be multiplied");
 	}
 
-	Matrix res(ma.nrow(), mb.ncol());
+	Matrix res(ma.nrow, mb.ncol);
 
-	for (size_t rowL = 0; rowL < ma.nrow(); rowL += tsize) {
+	for (size_t rowL = 0; rowL < ma.nrow; rowL += tsize) {
 		size_t rowR = rowL + tsize;
-		rowR = rowR > ma.nrow() ? ma.nrow() : rowR;	 // min
+		rowR = rowR > ma.nrow ? ma.nrow : rowR;	 // min
 
-		// for (size_t iL = 0; iL < ma.ncol(); iL += tsize) {
+		// for (size_t iL = 0; iL < ma.ncol; iL += tsize) {
 		// 	size_t iR = iL + tsize;
-		// 	iR = iR > ma.ncol() ? ma.ncol() : iR;  // min
+		// 	iR = iR > ma.ncol ? ma.ncol : iR;  // min
 
-		for (size_t colL = 0; colL < mb.ncol(); colL += tsize) {
+		for (size_t colL = 0; colL < mb.ncol; colL += tsize) {
 			size_t colR = colL + tsize;
-			colR = colR > mb.ncol() ? mb.ncol() : colR;	 // min
+			colR = colR > mb.ncol ? mb.ncol : colR;	 // min
 
 			// for (size_t i = iL; i < iR; i++) {
-			for (size_t i = 0; i < ma.ncol(); i++) {
+			for (size_t i = 0; i < ma.ncol; i++) {
 				for (size_t row = rowL; row < rowR; row++) {
 					for (size_t col = colL; col < colR; col++) {
 						res(row, col) += ma(row, i) * mb(i, col);
@@ -122,23 +132,23 @@ Matrix multiple_tile(Matrix& ma, Matrix& mb, size_t tsize) {
 }
 
 Matrix multiply_mkl(Matrix& A, Matrix& B) {
-	if (A.ncol() != B.nrow()) {
+	if (A.ncol != B.nrow) {
 		throw pybind11::value_error("Given matrices cannot be multiplied");
 	}
 
-	// double* A = new double[ma.nrow() * ma.ncol()];
-	// double* B = new double[mb.nrow() * mb.ncol()];
-	double* C = new double[A.nrow() * B.ncol()];
+	// double* A = new double[ma.nrow * ma.ncol];
+	// double* B = new double[mb.nrow * mb.ncol];
+	double* C = new double[A.nrow * B.ncol];
 
-	int m = A.nrow();
-	int k = A.ncol();
-	int n = B.ncol();
+	int m = A.nrow;
+	int k = A.ncol;
+	int n = B.ncol;
 	double alpha = 1.0, beta = 0.0;
 
 	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
 				m, n, k, alpha, A.data(), k, B.data(), n, beta, C, n);
 
-	Matrix res(A.nrow(), B.ncol());
+	Matrix res(A.nrow, B.ncol);
 	res.load2(C);
 
 	delete[] C;
@@ -163,10 +173,13 @@ PYBIND11_MODULE(_matrix, m) {
 			return mat.asstring();
 		})
 		.def("load", &Matrix::load)
-		.def("nrow", &Matrix::nrow)
-		.def("ncol", &Matrix::ncol);
+		// Ref: https://pybind11.readthedocs.io/en/stable/advanced/classes.html#operator-overloading
+		.def(pybind11::self == pybind11::self)
+		// Ref: https://pybind11.readthedocs.io/en/stable/classes.html#instance-and-static-fields
+		.def_readonly("nrow", &Matrix::nrow)
+		.def_readonly("ncol", &Matrix::ncol);
 
 	m.def("multiply_naive", &multiply_naive);
-	m.def("multiple_tile", &multiple_tile);
+	m.def("multiply_tile", &multiply_tile);
 	m.def("multiply_mkl", &multiply_mkl);
 }
--- hw3/Xi-Plus/test_matrix.py
+++ hw3/Xi-Plus/test_matrix.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pytest
-from _matrix import Matrix, multiple_tile, multiply_mkl, multiply_naive
+from _matrix import Matrix, multiply_mkl, multiply_naive, multiply_tile
 
 
 def test_check_naive_answer_by_numpy():
@@ -35,32 +35,27 @@ def test_check_naive_answer_by_numpy():
 
     for i in range(ndot.shape[0]):
         for j in range(ndot.shape[1]):
-            assert(ndot[i, j] == mdot[i, j])
+            assert ndot[i, j] == mdot[i, j]
 
 
 def test_check_answer_between_three_method():
     ma = Matrix(5, 10)
-    for i in range(ma.nrow()):
-        for j in range(ma.ncol()):
-            ma[i, j] = i * ma.ncol() + j + 1
+    for i in range(ma.nrow):
+        for j in range(ma.ncol):
+            ma[i, j] = i * ma.ncol + j + 1
     print('ma', ma)
     mb = Matrix(10, 20)
-    for i in range(mb.nrow()):
-        for j in range(mb.ncol()):
-            mb[i, j] = i * mb.ncol() + j + 1
+    for i in range(mb.nrow):
+        for j in range(mb.ncol):
+            mb[i, j] = i * mb.ncol + j + 1
     print('mb', mb)
 
     dot1 = multiply_naive(ma, mb)
     print('dot1', dot1)
-    dot2 = multiple_tile(ma, mb, 7)
+    dot2 = multiply_tile(ma, mb, 7)
     print('dot2', dot2)
     dot3 = multiply_mkl(ma, mb)
     print('dot3', dot3)
 
-    for i in range(dot1.nrow()):
-        for j in range(dot1.ncol()):
-            assert(dot1[i, j] == dot2[i, j])
-
-    for i in range(dot1.nrow()):
-        for j in range(dot1.ncol()):
-            assert(dot1[i, j] == dot3[i, j])
+    assert dot1 == dot2
+    assert dot1 == dot3
