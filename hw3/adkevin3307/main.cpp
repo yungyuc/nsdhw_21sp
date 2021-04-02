45257988e0bb4c8de2d803497b1f556a753dd973
adkevin3307 <adkevin3307@gmail.com>
2021-04-02 23:47:13 +0800

    test tile speed

--- hw3/adkevin3307/main.cpp
+++ hw3/adkevin3307/main.cpp
@@ -39,51 +39,14 @@ Matrix<T> multiply_tile(const Matrix<T>& m1, const Matrix<T>& m2, size_t size)
 
     Matrix<T> m3(m1.rows(), m2.cols());
 
-    size_t tile_rows = m1.rows() / size + (m1.rows() % size == 0 ? 0 : 1);
-    size_t tile_cols = m2.cols() / size + (m2.cols() % size == 0 ? 0 : 1);
-    size_t tile_inners = m1.cols() / size + (m1.cols() % size == 0 ? 0 : 1);
-
-    Matrix<T> tile_m1(size, size);
-    Matrix<T> tile_m2(size, size);
-
-    for (size_t row = 0; row < tile_rows; row++) {
-        for (size_t col = 0; col < tile_cols; col++) {
-            for (size_t inner = 0; inner < tile_inners; inner++) {
-                // copy
-                for (size_t i = 0; i < size; i++) {
-                    for (size_t j = 0; j < size; j++) {
-                        size_t index_row = row * size + i;
-                        size_t index_col = col * size + i;
-                        size_t index_inner = inner * size + j;
-
-                        if (index_row < m1.rows() && index_inner < m1.cols()) {
-                            tile_m1(i, j) = m1(index_row, index_inner);
-                        }
-                        else {
-                            tile_m1(i, j) = 0;
-                        }
-
-                        if (index_inner < m2.rows() && index_col < m2.cols()) {
-                            tile_m2(i, j) = m2(index_inner, index_col);
-                        }
-                        else {
-                            tile_m2(i, j) = 0;
-                        }
-                    }
-                }
-                // calculate
-                for (size_t i = 0; i < size; i++) {
-                    for (size_t j = 0; j < size; j++) {
-                        T tile_m3 = 0;
-                        size_t index_row = row * size + i;
-                        size_t index_col = col * size + j;
-
-                        if (index_row < m3.rows() && index_col < m3.cols()) {
-                            for (size_t k = 0; k < size; k++) {
-                                tile_m3 += tile_m1(i, k) * tile_m2(j, k);
-                            }
-
-                            m3(index_row, index_col) += tile_m3;
+    for (size_t row = 0; row < m1.rows(); row += size) {
+        for (size_t col = 0; col < m2.cols(); col += size) {
+            for (size_t inner = 0; inner < m1.cols(); inner += size) {
+                // tile
+                for (size_t k = inner; k < min(m1.cols(), inner + size); k++) {
+                    for (size_t i = row; i < min(m1.rows(), row + size); i++) {
+                        for (size_t j = col; j < min(m2.cols(), col + size); j++) {
+                            m3(i, j) += m1(i, k) * m2(k, j);
                         }
                     }
                 }
