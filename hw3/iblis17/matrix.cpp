c0ac53bd90ded9fe24d5f813dac418bfb27038d0
Iblis Lin <iblis@hs.ntnu.edu.tw>
2021-04-06 16:58:35 +0800

    repr

--- hw3/iblis17/matrix.cpp
+++ hw3/iblis17/matrix.cpp
@@ -1,6 +1,11 @@
 #include <algorithm>
 #include <iostream>
 #include <stddef.h>
+#include <string>
+
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
 
 
 class Matrix {
@@ -41,17 +46,23 @@ public:
     double  operator() (size_t i, size_t j) const { return this->_buf[i*_m + j]; }
     double& operator() (size_t i, size_t j)       { return this->_buf[i*_m + j]; }
 
-    size_t nrow() const { return _n; }
-    size_t ncol() const { return _m; }
+    size_t n() const { return _n; }
+    size_t m() const { return _m; }
 
-    Matrix multiply_naive()
-    {}
+    double* val() const { return _buf; }
 
-    Matrix multiply_tile()
-    {}
+    std::string repr() const {
+        std::string s = std::to_string(_n) + 'x' + std::to_string(_m) + '\n';
 
-    Matrix multiply_mkl()
-    {}
+        for (size_t i=0; i<_n; ++i)
+        {
+            for (size_t j=0; j<_m; ++j)
+                s += std::to_string(_buf[i*_m + j]) + ' ';
+            s += '\n';
+        }
+
+        return s;
+    }
 
 private:
     size_t _n;
@@ -59,3 +70,35 @@ private:
     double* _buf;
 };
 
+
+Matrix multiply_naive(Matrix& A, Matrix& B)
+{
+    if (A.m() != B.n())
+        throw(std::invalid_argument("invalid shape"));
+
+    Matrix C(A.n(), B.m());
+
+    for(size_t i=0; i<C.n(); ++i)
+        for(size_t j=0; j<C.m(); ++j)
+            for(size_t k=0; k<A.m(); ++k)
+                C(i, j) += A(i, k) * B(k, j);
+
+    return C;
+}
+
+Matrix multiply_tile(Matrix& A, Matrix& B)
+{}
+
+Matrix multiply_mkl(Matrix& A, Matrix& B)
+{}
+
+PYBIND11_MODULE(_matrix, m) {
+    m.def("multiply_naive", &multiply_naive, "");
+    py::class_<Matrix>(m, "Matrix")
+        .def(py::init<size_t, size_t>())
+        .def("n", &Matrix::n)
+        .def("m", &Matrix::m)
+        .def("val", &Matrix::val)
+        .def("__repr__", &Matrix::repr)
+        ;
+}
