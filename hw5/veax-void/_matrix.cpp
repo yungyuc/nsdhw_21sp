b27b06f537592edce1957cb6ede7eac54500cc41
veax-void <caxaev95@gmail.com>
2021-05-12 11:21:59 +0800

    changed makefile (tring to fix it v1)

--- hw5/veax-void/_matrix.cpp
+++ hw5/veax-void/_matrix.cpp
@@ -1,169 +1,121 @@
-#include <mkl.h>
-#include <pybind11/numpy.h>
-#include <pybind11/operators.h>
-#include <pybind11/pybind11.h>
+#include <iostream>
+#include <algorithm>
+#include "mkl.h"
+
 #include "_matrix.hpp"
 
-#include <mkl.h>
-
-std::ostream & operator<< (std::ostream & ostr, Matrix const & mat)
+// default contructor
+Matrix::Matrix(size_t nrow, size_t ncol)
+    : m_nrow(nrow), m_ncol(ncol), m_buffer(nrow * ncol, 0)
 {
-    for (size_t i=0; i<mat.nrow(); ++i)
-    {
-        ostr << std::endl << " ";
-        for (size_t j=0; j<mat.ncol(); ++j)
-        {
-            ostr << " " << std::setw(2) << mat(i, j);
-        }
-    }
-
-    return ostr;
 }
 
-Matrix operator*(Matrix const & mat1, Matrix const & mat2)
+// copy constructor
+Matrix::Matrix(Matrix const & other)
+    : m_nrow(other.m_nrow), m_ncol(other.m_ncol), m_buffer(other.m_nrow * other.m_ncol, 0)
 {
-    if (mat1.ncol() != mat2.nrow())
+    for(size_t i=0; i < m_nrow; ++i)
+    {
+        const size_t base_t = i*m_ncol;
+        const size_t base_s = i*other.m_ncol;
+        for (size_t j=0; j < m_ncol; ++j)
+            if (i >= other.m_nrow || j >= other.m_ncol) m_buffer.at(base_t + j) = 0;
+            else m_buffer.at(base_t + j) = other.m_buffer.at(base_s + j);
+    }
+}
+
+// move constructor
+Matrix::Matrix(Matrix && other)
+    : m_nrow(other.m_nrow), m_ncol(other.m_ncol), m_buffer(other.m_nrow * other.m_ncol, 0)
+{
+    other.m_buffer.swap(m_buffer);
+}
+
+Matrix::Matrix(std::vector<std::vector<double>> const & other)
+    : m_nrow(other.size()), m_ncol(other[0].size()), m_buffer(other.size() * other[0].size(), 0)
+{
+    for(const auto &v: other)
+        m_buffer.insert(m_buffer.end(), v.begin(), v.end()); 
+}
+
+inline void validate_multiplication(Matrix const &mat1, Matrix const &mat2)
+{
+    if (mat1.m_ncol != mat2.m_nrow)
     {
         throw std::out_of_range(
             "the number of first matrix column "
             "differs from that of second matrix row");
     }
+}
 
+Matrix multiply_naive(Matrix const &mat1, Matrix const &mat2)
+{
+    validate_multiplication(mat1, mat2);
+
+    // New matrix to be returned
     Matrix ret(mat1.nrow(), mat2.ncol());
 
     for (size_t i=0; i<ret.nrow(); ++i)
     {
         for (size_t k=0; k<ret.ncol(); ++k)
         {
-            double v = 0;
             for (size_t j=0; j<mat1.ncol(); ++j)
             {
-                v += mat1(i,j) * mat2(j,k);
+                ret(i, k) += mat1(i,j) * mat2(j,k);
             }
-            ret(i,k) = v;
         }
     }
-
     return ret;
-}
+};
 
-
-// Self Implementation starts here
-
-Matrix Matrix::multiply_naive (const Matrix &left, const Matrix& right) 
+Matrix multiply_mkl(Matrix const &mat1, Matrix const &mat2)
 {
-    if (left.ncol() != right.nrow()) {
-        throw std::out_of_range("Error Size");
-    }
+    validate_multiplication(mat1, mat2);
 
-    Matrix ret(left.nrow(), right.ncol());
+    // New matrix to be returned
+    Matrix ret(mat1.m_nrow, mat2.m_ncol);
 
-    for(int i = 0; i < ret.nrow(); ++i) {
-        for (int j = 0; j < ret.ncol(); ++j) {
-            double v = 0;
-            for(int k = 0; k < left.ncol(); ++k) {
-                v += left(i, k) * right(k, j);
-            }
-            ret(i, j) = v;
-        }
-    }
-
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+        mat1.m_nrow, mat2.m_ncol, mat1.m_ncol, 1.0,
+        mat1.m_buffer.data(), mat1.m_ncol,
+        mat2.m_buffer.data(), mat2.m_ncol,
+        0.0,
+        ret.m_buffer.data(), mat2.m_ncol
+    );
     return ret;
-}
+};
 
-Matrix Matrix::multiply_tiling (const Matrix &left, const Matrix& right, int cacheline) 
+Matrix multiply_tile(Matrix const &mat1, Matrix const &mat2, const int tsize)
 {
-    if (left.ncol() != right.nrow()) {
-        throw std::out_of_range("Error Size");
-    }
+    validate_multiplication(mat1, mat2);
 
-    Matrix ret(left.nrow(), right.ncol());
-    for(size_t i = 0; i < left.nrow(); ++i) {
-        for (size_t j = 0; j < right.ncol(); ++j) {
-            ret(i, j) = 0.0; // explicit clean the matrix since we "add" value to ret not "assign"
-        }
-    }
+    const size_t f_row = mat1.m_nrow;
+    const size_t f_col = mat1.m_ncol;
+    const size_t s_col = mat2.m_ncol;
 
-    const int blocksize = cacheline / sizeof(double);
+    Matrix ret(f_row, s_col);
 
-    for(int i = 0; i < ret.nrow(); i += blocksize) {
-        const bool isExceedBound = ((i + blocksize) > ret.nrow());
-        const int index_i = isExceedBound ? ret.nrow() : (i + blocksize); // clamp the size if out of bound
-        
-        for (int j = 0; j < ret.ncol(); j += blocksize) {
-            const bool isExceedBound = ((j + blocksize) > ret.ncol());
-            const int index_j = isExceedBound ? ret.ncol() : (j + blocksize);
+    const size_t tile = tsize;
 
-            for (int k = 0; k < left.ncol(); k += blocksize) {
-                const bool isExceedBound = ((k + blocksize) > left.ncol());
-                const int index_k = isExceedBound ? left.ncol() : (k + blocksize);
+    for (size_t k_tile = 0; k_tile < f_col; k_tile += tile) {
+        const size_t k_tile_cur = std::min( f_col, (k_tile + tile) );
 
-                // do multiplication in blocks
-                for (int tile_i = i; tile_i < index_i; ++tile_i) {
-                    for (int tile_j = j; tile_j < index_j; ++tile_j) {
-                        double v = 0;
-                        for (int tile_k = k; tile_k < index_k; ++tile_k) {
-                            v += left(tile_i, tile_k) * right(tile_k, tile_j);
+        for (size_t i_tile = 0; i_tile < f_row; i_tile += tile) {
+            const size_t i_tile_cur = std::min( f_row, (i_tile + tile) );
+
+            for (size_t j_tile = 0; j_tile < s_col; j_tile += tile) {
+                const size_t j_tile_cur = std::min( s_col, (j_tile + tile) );
+
+                for (size_t k = k_tile; k < k_tile_cur; ++k) {
+                    for (size_t i = i_tile; i < i_tile_cur; ++i) {
+                        const double left = mat1(i, k);
+                        for (size_t j = j_tile; j < j_tile_cur; ++j) {
+                            ret(i, j) += left * mat2(k, j);
                         }
-                        ret(tile_i, tile_j) += v; // use += instead of =
                     }
                 }
-
             }
         }
-    }
-
-    return ret;
-}
-
-Matrix Matrix::multiply_mkl (const Matrix &left, const Matrix& right) 
-{
-    if (left.ncol() != right.nrow()) {
-        throw std::out_of_range("Error Size");
-    }
-    
-    Matrix ret(left.nrow(), right.ncol());
-
-    // Ref: https://software.intel.com/en-us/mkl-tutorial-c-multiplying-matrices-using-dgemm
-
-    int m = left.nrow();
-    int k = left.ncol();
-    int n = right.ncol();
-
-    #ifdef ROW_MAJOR
-        cblas_dgemm(        
-            CblasRowMajor,        
-            CblasNoTrans,   //
-            CblasNoTrans,   //     
-            m,              // M
-            n,              // N
-            k,              // K
-            1.0,            // ALPHA,
-            left.data(),    // A
-            left.ncol(),    // LDA,
-            right.data(),   // B
-            right.ncol(),   // LDB
-            0.0,            // BETA,
-            ret.data(),     // C
-            ret.ncol()      // LDC
-        );
-    #else
-        cblas_dgemm(        
-            CblasColMajor,        
-            CblasNoTrans,   //
-            CblasNoTrans,   //     
-            m,              // M
-            n,              // N
-            k,              // K
-            1.0,            // ALPHA,
-            left.data(),    // A
-            left.nrow(),    // LDA,
-            right.data(),   // B
-            right.nrow(),   // LDB        
-            0.0,            // BETA,
-            ret.data(),     // C
-            ret.nrow()      // LDC
-        );
-    #endif
+    }    
     return ret;
 }
--- hw5/veax-void/_matrix.hpp
+++ hw5/veax-void/_matrix.hpp
@@ -1,147 +1,73 @@
-#pragma once
+#ifndef __MATRIX_H
+#define __MATRIX_H
 
 #include <iostream>
-#include <iomanip>
 #include <vector>
-#include <stdexcept>
-#include <cstring>
 
 class Matrix {
 
-    public:
+public:
 
-        Matrix() = default;
+    Matrix(size_t nrow, size_t ncol);
+    Matrix(Matrix const & other);
+    Matrix(Matrix && other);
+    Matrix(std::vector<std::vector<double>> const & other);
 
-        Matrix(size_t nrow, size_t ncol)
-        : m_nrow(nrow), m_ncol(ncol)
+    ~Matrix() = default;
+
+    friend void validate_multiplication(const Matrix &mat1, const Matrix &mat2);
+    friend Matrix multiply_naive(const Matrix &mat1, const Matrix &mat2);
+    friend Matrix multiply_mkl(const Matrix &mat1, const Matrix &mat2);
+    friend Matrix multiply_tile(const Matrix &mat1, const Matrix &mat2, int lsize);
+
+    size_t index(size_t row, size_t col) const { return row*m_ncol + col; }
+    double   operator() (size_t row, size_t col) const { return m_buffer[ index(row, col) ]; }
+    double & operator() (size_t row, size_t col)       { return m_buffer[ index(row, col) ]; }
+    double   operator() (size_t idx) const { return m_buffer[ idx ]; }
+    double & operator() (size_t idx)       { return m_buffer[ idx ]; }
+
+    bool operator== (Matrix const &other) const
+    {
+        if (this == &other) return true;
+        if (m_nrow != other.m_nrow || m_ncol != other.m_ncol) return false;
+        if (m_buffer == other.m_buffer) return true;
+        else return false;
+    }
+
+    void save(Matrix &mat, size_t it, size_t jt)
+    {
+        const size_t ncol = mat.ncol();
+
+        for (size_t i=0; i<m_nrow; ++i)
         {
-            reset_buffer(nrow, ncol);
-        }
-
-        Matrix(size_t nrow, size_t ncol, std::vector<double> const & vec)
-        : m_nrow(nrow), m_ncol(ncol)
-        {
-            reset_buffer(nrow, ncol);
-            (*this) = vec;
-        }
-
-        Matrix & operator=(std::vector<double> const & vec)
-        {
-            if (size() != vec.size())
+            size_t base_s = i*m_ncol;
+            size_t base_t = (it+i)*ncol+jt;
+            for (size_t j=0; j<m_ncol; ++j)
             {
-                throw std::out_of_range("number of elements mismatch");
-            }
-
-            size_t k = 0;
-            for (size_t i=0; i<m_nrow; ++i)
-            {
-                for (size_t j=0; j<m_ncol; ++j)
-                {
-                    (*this)(i,j) = vec[k];
-                    ++k;
-                }
-            }
-
-            return *this;
-        }
-
-        Matrix(Matrix const & other)
-        : m_nrow(other.m_nrow), m_ncol(other.m_ncol)
-        {
-            reset_buffer(other.m_nrow, other.m_ncol);
-            for (size_t i=0; i<m_nrow; ++i)
-            {
-                for (size_t j=0; j<m_ncol; ++j)
-                {
-                    (*this)(i,j) = other(i,j);
-                }
+                mat(base_t + j) = m_buffer.at(base_s + j);
             }
         }
+    }
 
-        Matrix & operator=(Matrix const & other)
-        {
-            if (this == &other) { return *this; }
-            if (m_nrow != other.m_nrow || m_ncol != other.m_ncol)
-            {
-                reset_buffer(other.m_nrow, other.m_ncol);
-            }
-            for (size_t i=0; i<m_nrow; ++i)
-            {
-                for (size_t j=0; j<m_ncol; ++j)
-                {
-                    (*this)(i,j) = other(i,j);
-                }
-            }
-            return *this;
-        }
+    double *data() { return m_buffer.data(); }
+    size_t nrow() const { return m_nrow; }
+    size_t ncol() const { return m_ncol; }
 
-        Matrix(Matrix && other)
-        : m_nrow(other.m_nrow), m_ncol(other.m_ncol)
-        {
-            reset_buffer(0, 0);
-            std::swap(m_nrow, other.m_nrow);
-            std::swap(m_ncol, other.m_ncol);
-            std::swap(m_buffer, other.m_buffer);
-        }
+private:
 
-        Matrix & operator=(Matrix && other)
-        {
-            if (this == &other) { return *this; }
-            reset_buffer(0, 0);
-            std::swap(m_nrow, other.m_nrow);
-            std::swap(m_ncol, other.m_ncol);
-            std::swap(m_buffer, other.m_buffer);
-            return *this;
-        }
+    size_t m_nrow;
+    size_t m_ncol;
+    std::vector<double> m_buffer;
 
-        ~Matrix()
-        {
-            reset_buffer(0, 0);
-        }
-
-        double   operator() (size_t row, size_t col) const { return m_buffer[index(row, col)]; }
-        double & operator() (size_t row, size_t col)       { return m_buffer[index(row, col)]; }
-
-        static Matrix multiply_naive (const Matrix &left, const Matrix& right);
-        static Matrix multiply_tiling (const Matrix &left, const Matrix& right, int cacheline);
-        static Matrix multiply_mkl (const Matrix &left, const Matrix& right);
-
-        friend std::ostream & operator<< (std::ostream & ostr, Matrix const & mat);
-        friend Matrix operator*(Matrix const & mat1, Matrix const & mat2);
-
-        size_t nrow() const { return m_nrow; }
-        size_t ncol() const { return m_ncol; }
-
-        size_t size() const { return m_nrow * m_ncol; }
-        double buffer(size_t i) const { return m_buffer[i]; }
-        std::vector<double> buffer_vector() const { return std::vector<double>(m_buffer, m_buffer+size()); }
-
-        double * data() const { return m_buffer; }
-
-    private:
-
-        size_t index(size_t row, size_t col) const
-        {
-            #ifdef ROW_MAJOR
-                return row * m_ncol + col;
-            #else
-                return row + col * m_nrow; 
-            #endif
-        }
-
-        void reset_buffer(size_t nrow, size_t ncol)
-        {
-            if (m_buffer) { delete[] m_buffer; }
-            const size_t nelement = nrow * ncol;
-            if (nelement) { m_buffer = new double[nelement]; }
-            else          { m_buffer = nullptr; }
-            m_nrow = nrow;
-            m_ncol = ncol;
-
-            memset(m_buffer, 0, sizeof(double) * nelement);
-        }
-
-        size_t m_nrow = 0;
-        size_t m_ncol = 0;
-        double * m_buffer = nullptr;
 };
+
+void validate_multiplication(Matrix const &mat1, Matrix const &mat2);
+Matrix multiply_naive(Matrix const &mat1, Matrix const &mat2);
+Matrix multiply_mkl(Matrix const &mat1, Matrix const &mat2);
+Matrix multiply_tile(Matrix const &mat1, Matrix const &mat2, int tsize);
+
+std::size_t bytes();
+std::size_t allocated();
+std::size_t deallocated();
+
+#endif
