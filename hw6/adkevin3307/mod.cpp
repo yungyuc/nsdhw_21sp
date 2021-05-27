c4baeb5c1a2670088b28e8c459d9c97d81a16382
adkevin3307 <adkevin3307@gmail.com>
2021-05-27 22:31:08 +0800

    fix mod.cpp include header

--- hw6/adkevin3307/mod.cpp
+++ hw6/adkevin3307/mod.cpp
@@ -35,124 +35,99 @@
 #include <atomic>
 #include <iostream>
 
-#include "matrix.hpp"
+#include "Matrix.hpp"
 
 #ifdef __GNUG__
-#  define PYTHON_WRAPPER_VISIBILITY __attribute__((visibility("hidden")))
+#define PYTHON_WRAPPER_VISIBILITY __attribute__((visibility("hidden")))
 #else
-#  define PYTHON_WRAPPER_VISIBILITY
+#define PYTHON_WRAPPER_VISIBILITY
 #endif
 
-namespace python
-{
+namespace python {
 
-struct calltag {};
+    struct calltag {
+    };
 
 } /* end namespace python */
 
-namespace pybind11
-{
-namespace detail
-{
+namespace pybind11 {
+    namespace detail {
 
-template<> struct process_attribute<python::calltag>
-  : process_attribute_default<python::calltag>
-{
+        template<>
+        struct process_attribute<python::calltag>
+            : process_attribute_default<python::calltag> {
+            static void postcall(function_call& call, handle&)
+            {
+                mark(call);
+            }
 
-    static void postcall(function_call & call, handle &)
-    {
-        mark(call);
-    }
+        private:
+            static void mark(function_call const& call)
+            {
+                object o = module::import("_matrix");
+                std::string name = std::string(str(call.func.scope.attr("__name__")));
+                name += std::string(call.func.name) + "_post_count";
+                const size_t count = getattr(o, name.c_str(), int_(0)).cast<size_t>() + 1;
+                setattr(o, name.c_str(), int_(count));
+            }
+        };
 
-private:
-
-    static void mark(function_call const & call)
-    {
-        object o = module::import("_matrix");
-        std::string name = std::string(str(call.func.scope.attr("__name__")));
-        name += std::string(call.func.name) + "_post_count";
-        const size_t count = getattr(o, name.c_str(), int_(0)).cast<size_t>() + 1;
-        setattr(o, name.c_str(), int_(count));
-    }
-
-};
-
-} /* end namespace detail */
+    } /* end namespace detail */
 } /* end namespace pybind11 */
 
-namespace python
-{
+namespace python {
 
-/**
+    /**
  * Helper template for pybind11 class wrappers.
  */
-template
-<
-    class Wrapper
-  , class Wrapped
-  , class Holder = std::unique_ptr<Wrapped>
-  , class WrappedBase = Wrapped
->
-/*
+    template<
+        class Wrapper, class Wrapped, class Holder = std::unique_ptr<Wrapped>, class WrappedBase = Wrapped>
+    /*
  * Use CRTP to detect type error during compile time.
  */
-class
-PYTHON_WRAPPER_VISIBILITY
-WrapBase
-{
+    class
+        PYTHON_WRAPPER_VISIBILITY
+            WrapBase {
+    public:
+        using wrapper_type = Wrapper;
+        using wrapped_type = Wrapped;
+        using wrapped_base_type = WrappedBase;
+        using holder_type = Holder;
+        using root_base_type = WrapBase<
+            wrapper_type, wrapped_type, holder_type, wrapped_base_type>;
+        using class_ = typename std::conditional_t<
+            std::is_same<Wrapped, WrappedBase>::value, pybind11::class_<wrapped_type, holder_type>, pybind11::class_<wrapped_type, wrapped_base_type, holder_type> >;
 
-public:
+        static wrapper_type& commit(pybind11::module& mod)
+        {
+            static wrapper_type derived(mod);
+            return derived;
+        }
 
-    using wrapper_type = Wrapper;
-    using wrapped_type = Wrapped;
-    using wrapped_base_type = WrappedBase;
-    using holder_type = Holder;
-    using root_base_type = WrapBase
-    <
-        wrapper_type
-      , wrapped_type
-      , holder_type
-      , wrapped_base_type
-    >;
-    using class_ = typename std::conditional_t
-    <
-        std::is_same< Wrapped, WrappedBase >::value
-      , pybind11::class_< wrapped_type, holder_type >
-      , pybind11::class_< wrapped_type, wrapped_base_type, holder_type >
-    >;
+        static wrapper_type& commit(pybind11::module& mod, char const* pyname, char const* pydoc)
+        {
+            static wrapper_type derived(mod, pyname, pydoc);
+            return derived;
+        }
 
-    static wrapper_type & commit(pybind11::module & mod)
-    {
-        static wrapper_type derived(mod);
-        return derived;
-    }
-
-    static wrapper_type & commit(pybind11::module & mod, char const * pyname, char const * pydoc)
-    {
-        static wrapper_type derived(mod, pyname, pydoc);
-        return derived;
-    }
-
-    WrapBase() = delete;
-    WrapBase(WrapBase const & ) = default;
-    WrapBase(WrapBase       &&) = delete;
-    WrapBase & operator=(WrapBase const & ) = default;
-    WrapBase & operator=(WrapBase       &&) = delete;
-    ~WrapBase() = default;
+        WrapBase() = delete;
+        WrapBase(WrapBase const&) = default;
+        WrapBase(WrapBase&&) = delete;
+        WrapBase& operator=(WrapBase const&) = default;
+        WrapBase& operator=(WrapBase&&) = delete;
+        ~WrapBase() = default;
 
 #define DECL_PYBIND_CLASS_METHOD_UNTAGGED(METHOD) \
-    template< class... Args > \
-    /* NOLINTNEXTLINE(bugprone-macro-parentheses) */ \
-    wrapper_type & METHOD(Args&&... args) \
+    template<class... Args> /* NOLINTNEXTLINE(bugprone-macro-parentheses) */ \
+    wrapper_type& METHOD(Args&&... args) \
     { \
         m_cls.METHOD(std::forward<Args>(args)...); \
         return *static_cast<wrapper_type*>(this); \
     }
 
 #define DECL_PYBIND_CLASS_METHOD_TAGGED(METHOD) \
-    template< class... Args > \
-    /* NOLINTNEXTLINE(bugprone-macro-parentheses) */ \
-    wrapper_type & METHOD ## _tagged(Args&&... args) \
+    template<class... Args> /* NOLINTNEXTLINE(bugprone-macro-parentheses) */ \
+    wrapper_type& METHOD##_tagged(Args&&... args) \
     { \
         m_cls.METHOD(std::forward<Args>(args)..., calltag()); \
         return *static_cast<wrapper_type*>(this); \
@@ -162,83 +137,72 @@ public:
     DECL_PYBIND_CLASS_METHOD_UNTAGGED(METHOD) \
     DECL_PYBIND_CLASS_METHOD_TAGGED(METHOD)
 
-    DECL_PYBIND_CLASS_METHOD(def)
-    DECL_PYBIND_CLASS_METHOD(def_static)
+        DECL_PYBIND_CLASS_METHOD(def)
+        DECL_PYBIND_CLASS_METHOD(def_static)
 
-    DECL_PYBIND_CLASS_METHOD(def_readwrite)
-    DECL_PYBIND_CLASS_METHOD(def_readonly)
-    DECL_PYBIND_CLASS_METHOD(def_readwrite_static)
-    DECL_PYBIND_CLASS_METHOD(def_readonly_static)
+        DECL_PYBIND_CLASS_METHOD(def_readwrite)
+        DECL_PYBIND_CLASS_METHOD(def_readonly)
+        DECL_PYBIND_CLASS_METHOD(def_readwrite_static)
+        DECL_PYBIND_CLASS_METHOD(def_readonly_static)
 
-    DECL_PYBIND_CLASS_METHOD(def_property)
-    DECL_PYBIND_CLASS_METHOD(def_property_static)
-    DECL_PYBIND_CLASS_METHOD(def_property_readonly)
-    DECL_PYBIND_CLASS_METHOD(def_property_readonly_static)
+        DECL_PYBIND_CLASS_METHOD(def_property)
+        DECL_PYBIND_CLASS_METHOD(def_property_static)
+        DECL_PYBIND_CLASS_METHOD(def_property_readonly)
+        DECL_PYBIND_CLASS_METHOD(def_property_readonly_static)
 
 #undef DECL_PYBIND_CLASS_METHOD
 #undef DECL_PYBIND_CLASS_METHOD_TAGGED
 #undef DECL_PYBIND_CLASS_METHOD_UNTAGGED
 
-    class_ & cls() { return m_cls; }
+        class_& cls()
+        {
+            return m_cls;
+        }
 
-protected:
+    protected:
+        template<typename... Extra>
+        WrapBase(pybind11::module& mod, char const* pyname, char const* pydoc, const Extra&... extra)
+            : m_cls(mod, pyname, pydoc, extra...)
+        {
+        }
 
-    template <typename... Extra>
-    WrapBase(pybind11::module & mod, char const * pyname, char const * pydoc, const Extra & ... extra)
-      : m_cls(mod, pyname, pydoc, extra ...)
-    {}
+    private:
+        class_ m_cls;
 
-private:
+    }; /* end class WrapBase */
 
-    class_ m_cls;
-
-}; /* end class WrapBase */
-
-/**
+    /**
  * Wrapper for the Matrix class.
  */
-class
-PYTHON_WRAPPER_VISIBILITY
-WrapMatrix
-  : public WrapBase< WrapMatrix, Matrix >
-{
+    class
+        PYTHON_WRAPPER_VISIBILITY
+            WrapMatrix
+        : public WrapBase<WrapMatrix, Matrix> {
+        friend root_base_type;
 
-    friend root_base_type;
+        WrapMatrix(pybind11::module& mod, char const* pyname, char const* pydoc)
+            : root_base_type(mod, pyname, pydoc, pybind11::buffer_protocol())
+        {
+            namespace py = pybind11;
 
-    WrapMatrix(pybind11::module & mod, char const * pyname, char const * pydoc)
-      : root_base_type(mod, pyname, pydoc, pybind11::buffer_protocol())
-    {
+            (*this)
+                // The Python constructor will be counted!
+                .def_tagged(py::init<size_t, size_t>())
+                .def_property("array", [](Matrix &m) { return py::array_t<double>({m.nrow(), m.ncol()}, {sizeof(double) * m.ncol(), sizeof(double)}, m.data(), py::cast(m)); }, nullptr)
+                .def_property_readonly("nrow", &Matrix::nrow)
+                .def_property_readonly("ncol", &Matrix::nrow)
+                .def("__eq__", [](Matrix const& self, Matrix const& other) { return self == other; })
+                .def(
+                    "__getitem__", [](Matrix const& self, std::tuple<size_t, size_t> idx) { return self(std::get<0>(idx), std::get<1>(idx)); })
+                .def(
+                    "__setitem__", [](Matrix& self, std::tuple<size_t, size_t> idx, double value) { return self(std::get<0>(idx), std::get<1>(idx)) = value; });
 
-        namespace py = pybind11;
+            mod.def("multiply_mkl", &multiply_mkl);
+            mod.def("multiply_naive", &multiply_naive);
+            mod.def("multiply_tile", &multiply_tile);
+        }
 
-        (*this)
-            // The Python constructor will be counted!
-            .def_tagged(py::init<size_t, size_t>())
-            .def_property("array", [](Matrix &m) { return py::array_t<double>({ m.nrow(), m.ncol() }, { sizeof(double) * m.ncol(), sizeof(double) }, m.data(), py::cast(m)); }, nullptr)
-            .def_property_readonly("nrow", &Matrix::nrow)
-            .def_property_readonly("ncol", &Matrix::nrow)
-            .def("__eq__", [](Matrix const & self, Matrix const & other) { return self == other; })
-            .def
-            (
-                "__getitem__"
-              , [](Matrix const & self, std::tuple<size_t, size_t> idx)
-                { return self(std::get<0>(idx), std::get<1>(idx)); }
-            )
-            .def
-            (
-                "__setitem__"
-              , [](Matrix & self, std::tuple<size_t, size_t> idx, double value)
-                { return self(std::get<0>(idx), std::get<1>(idx)) = value; }
-            )
-        ;
-
-        mod.def("multiply_mkl", &multiply_mkl);
-        mod.def("multiply_naive", &multiply_naive);
-        mod.def("multiply_tile", &multiply_tile);
-
-    }
-
-}; /* end class WrapMatrix */
+    }; /* end class WrapMatrix */
 
 } /* end namespace python */
 
